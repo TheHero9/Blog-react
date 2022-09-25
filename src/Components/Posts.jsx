33bc7185186072b1e7d906ecd78e1364d6cb9bdc
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2022-09-25 23:53:27 +0300

    Update Posts.jsx

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -68,7 +68,7 @@ export default function Posts(props) {
               Lorem Ipsum has been the industry's standard dummy text ever since the 
               1500s, when an unknown printer took a galley of type and scrambled it to 
               make a type specimen book. It has survived not only five centuries, but also the leap into 
-              electronic typesetting, Leoki is gay. It was popularised in the 1960s with 
+              electronic typ. It was popularised in the 1960s with 
               the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
               publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Card.Text>
               <Button onClick={() => handleClick("belgrade")} >{isActiveBelgrade ? "Close" : 'Expand'}</Button>
