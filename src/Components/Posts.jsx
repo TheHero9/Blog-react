85373107a8e6ccd90f5b47dd1554a7b720b41846
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2022-10-04 22:00:41 +0300

    Update Posts.jsx

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -67,7 +67,7 @@ export default function Posts(props) {
               text of the printing and typesetting industry. 
               Lorem Ipsum has been the industry's standard dummy text ever since the 
               1500s, when an unknown printer took a galley of type and scrambled it to 
-              make a type specimen book. It has survived not only five centuries, but also the leap into 
+              make a type specimen book. It has survived not only fi
               the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
               publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Card.Text>
               <Button onClick={() => handleClick("belgrade")} >{isActiveBelgrade ? "Close" : 'Expand'}</Button>
